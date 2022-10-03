f6ea64db037f1e83d1415fb0a5d44e6d9ca8595d
Rozen <t.a.denboef@gmail.com>
2022-10-03 15:31:42 +0200

    Add project files.

---
+++ Crossplay/Crossplay.cs
@@ -0,0 +1,136 @@
+﻿using Auxiliary.Configuration;
+using Auxiliary.Packets;
+using Terraria;
+using Terraria.Localization;
+using TerrariaApi.Server;
+using TShockAPI.Hooks;
+
+namespace Crossplay
+{
+    [ApiVersion(2, 1)]
+    public class Crossplay : TerrariaPlugin
+    {
+        private readonly List<int> _allowedVersions = new() 
+        { 
+            269, 
+            270, 
+            271 
+        };
+
+        private readonly int[] _clientVersions = new int[Main.maxPlayers];
+
+        private static int _serverVersion;
+
+        public override string Name 
+            => "Crossplay";
+
+        public override string Author 
+            => "Armano den Boef (Rozen4334)";
+
+        public override string Description 
+            => "Enables crossplay for Terraria mobile <> PC";
+
+        public override Version Version 
+            => new(1, 0);
+
+        public Crossplay(Main game)
+            : base(game)
+        {
+            Order = -1;
+        }
+
+        public override void Initialize()
+        {
+            Configuration<CrossplaySettings>.Load("Crossplay");
+
+            GeneralHooks.ReloadEvent += (x) =>
+            {
+                Configuration<CrossplaySettings>.Load("Crossplay");
+                x.Player.SendSuccessMessage("[Crossplay] Reloaded configuration.");
+            };
+
+            if (Configuration<CrossplaySettings>.Settings.UseFakeVersion)
+                _serverVersion = Configuration<CrossplaySettings>.Settings.FakeVersion;
+            else
+                _serverVersion = Main.curRelease;
+
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData, int.MaxValue);
+            ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
+        }
+
+        private void OnGetData(GetDataEventArgs args)
+        {
+            MemoryStream stream = new(args.Msg.readBuffer, args.Index, args.Length);
+
+            int index = args.Msg.whoAmI;
+            using BinaryReader reader = new(stream);
+
+            switch (args.MsgID)
+            {
+                case PacketTypes.ConnectRequest:
+                    {
+                        string clientVersion = reader.ReadString();
+
+                        if (!int.TryParse(clientVersion.AsSpan(clientVersion.Length - 3), out int versionNum))
+                            return;
+
+                        if (versionNum == _serverVersion)
+                            return;
+
+                        if (!_allowedVersions.Contains(versionNum) && !Configuration<CrossplaySettings>.Settings.UseFakeVersion)
+                            return;
+
+                        _clientVersions[index] = versionNum;
+                        NetMessage.SendData(9, args.Msg.whoAmI, -1, NetworkText.FromLiteral("Fixing Version..."), 1);
+
+                        byte[] connectRequest = new PacketFactory()
+                            .SetType(1)
+                            .PackString($"Terraria{_serverVersion}")
+                            .GetByteData();
+
+                        Log($"[Crossplay] Changing version of index {args.Msg.whoAmI} from {ParseVersion(versionNum)} => {ParseVersion(_serverVersion)}", ConsoleColor.Magenta);
+
+                        Buffer.BlockCopy(connectRequest, 0, args.Msg.readBuffer, args.Index - 3, connectRequest.Length);
+                    }
+                    break;
+                case PacketTypes.PlayerInfo:
+                    {
+                        var length = args.Length - 1;
+                        var bitsbyte = (BitsByte)args.Msg.readBuffer[length];
+
+                        if (Main.GameMode == 3 && !bitsbyte[3])
+                        {
+                            bitsbyte[0] = false;
+                            bitsbyte[1] = false;
+
+                            bitsbyte[3] = true;
+
+                            args.Msg.readBuffer[length] = bitsbyte;
+
+                            Log($"[Crossplay] {(bitsbyte[3] ? "Enabled" : "Disabled")} journeymode for index {args.Msg.whoAmI}.", ConsoleColor.Magenta);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void OnLeave(LeaveEventArgs args)
+            => _clientVersions[args.Who] = 0;
+
+        private static void Log(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static string ParseVersion(int version)
+            => version switch
+            {
+                269 => "v1.4.4",
+                270 => "v1.4.4.1",
+                271 => "v1.4.4.2",
+                _ => $"Unknown{version}",
+            };
+    }
+}
---
+++ Crossplay/CrossplaySettings.cs
@@ -0,0 +1,22 @@
+﻿using Auxiliary.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Crossplay
+{
+    public class CrossplaySettings : ISettings
+    {
+        [JsonPropertyName("enable-classic-support")]
+        public bool EnableClassicSupport { get; set; } = true;
+
+        [JsonPropertyName("use-fake-version")]
+        public bool UseFakeVersion { get; set; } = true;
+
+        [JsonPropertyName("fake-version")]
+        public int FakeVersion { get; set; } = 271;
+    }
+}
